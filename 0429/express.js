5ec54a28bef56559e3c7ee9e92fded6f584c9516
startergate <startergate36@gmail.com>
2020-05-20 19:50:09 +0900

    0519

--- 0429/express.js
+++ 0429/express.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use((req, res, next) => {
+    console.log('middleware 1')
+    next()
+});
+
+app.use((req, res, next) => {
+    console.log('middleware 2')
+    next()
+});
 
 app.get('/', (req, res) => {
     res.send('hello');
@@ -40,6 +53,11 @@ app.get('/siteMove', (req, res) => {
             path = 'https://gsm.gen.hs.kr';
     }
     res.redirect(path);
+});
+
+app.post('/loginCheck', (req, res) => {
+    if (req.body.id === 'smart' && req.body.pw === '123') req.redirect('/Users/choi/CodingData/Learn-Node/0429/login_s.html')
+    else req.redirect('/Users/choi/CodingData/Learn-Node/0429/login_f.html')
 });
 
 app.listen(3000, _ => {
